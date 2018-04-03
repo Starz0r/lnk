1625335499d0ace0bf192a92e2729989394a58b0
Starz0r <starz0rdesign@gmail.com>
2018-04-03 01:51:20 -0500

    Better argument constructors

--- src/main.rs
+++ src/main.rs
@@ -14,28 +14,31 @@ fn main() {
         {
             if (argv.len() == 4)
             {
-                let allocated_string = String::from("/d");
+                println!("Argument 1: {}", &argv[0]);
+                println!("Argument 2: {}", &argv[1]);
+                println!("Argument 3: {}", &argv[2]);
+                println!("Argument 4: {}", &argv[3]);
 
                 match argv[1].to_lowercase().as_str()
                 {
                     // Symbolic Link
-                    "/d" =>
+                    "--soft" =>
                     {
 
                     },
 
                     // Hard Link
-                    "/h" =>
+                    "--hard" =>
                     {
                         match fs::hard_link(&argv[2], &argv[3])
                         {
                             Ok(_) => println!("Hard Link created at {}, with destination {}", &argv[2], &argv[3]),
-                            Err(_) => println!("The given paths were not files or non-existant."),
+                            Err(_) => panic!("The given paths were not files or non-existent."),
                         }
                     },
 
                     // Soft Link
-                    "/j" =>
+                    "--junction" =>
                     {
 
                     },
@@ -43,6 +46,10 @@ fn main() {
                     // Invalid Link
                     _ => {},
                 }
+            }
+            else
+            {
+                panic!("Not enough arguments.");
             }
         },
         None => panic!("Could not get home directory, aborting."),
