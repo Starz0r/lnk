8b0dccda6413e95c70dd69e12bfe5f9852def3db
Starz0r <starz0rdesign@gmail.com>
2018-04-03 01:14:44 -0500

    Hard Links can be produced

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,52 @@
 extern crate simplelog;
 extern crate winapi;
 
+use std::env;
+use std::fs;
+
 fn main() {
+    let argv: Vec<String> = env::args().collect();
+    let path = env::home_dir();
+
+    match path
+    {
+        Some(p) =>
+        {
+            if (argv.len() == 4)
+            {
+                let allocated_string = String::from("/d");
+
+                match argv[1].to_lowercase().as_str()
+                {
+                    // Symbolic Link
+                    "/d" =>
+                    {
+
+                    },
+
+                    // Hard Link
+                    "/h" =>
+                    {
+                        match fs::hard_link(&argv[2], &argv[3])
+                        {
+                            Ok(_) => println!("Hard Link created at {}, with destination {}", &argv[2], &argv[3]),
+                            Err(_) => println!("The given paths were not files or non-existant."),
+                        }
+                    },
+
+                    // Soft Link
+                    "/j" =>
+                    {
+
+                    },
+
+                    // Invalid Link
+                    _ => {},
+                }
+            }
+        },
+        None => panic!("Could not get home directory, aborting."),
+    }
+
     println!("Hello, world!");
 }
