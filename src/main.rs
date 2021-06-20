e4225ed68b422d13766d8232ebaa4db3a893cb3f
Starz0r <starz0r@starz0r.com>
2021-06-19 21:51:43 -0500

    Properly support junctions and swap wording around the terminology used

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use {owo_colors::OwoColorize, structopt::StructOpt};
 #[derive(StructOpt, Debug)]
 #[structopt(name = "mklink")]
 struct CliOpts {
-    #[structopt(short = "o", long = "target")]
+    #[structopt(short = "t", long = "link")]
     src: PathBuf,
 
-    #[structopt(short = "t", long = "link")]
+    #[structopt(short = "o", long = "target")]
     dst: PathBuf,
 
-    #[structopt(short = "j", long = "junction")]
+    #[structopt(short = "s", long = "soft")]
     soft: bool,
 
     #[structopt(short, long)]
@@ -19,6 +19,9 @@ struct CliOpts {
 
     #[structopt(short = "d", long = "directory")]
     symbolic: bool,
+
+    #[structopt(short = "j", long = "junction")]
+    junction: bool,
 }
 
 const FILE_ERR: &str = "File was a path or non-existent.";
@@ -28,47 +31,61 @@ const LINK_TYPE_ERR: &str = "Multiple or no link type(s) were specified or inval
 fn main() {
     let argv = CliOpts::from_args();
 
-    let cmd = (argv.soft as u8 * 1) + (argv.hard as u8 * 4) + (argv.symbolic as u8 * 8);
+    let cmd = (argv.soft as u8 * 1)
+        + (argv.hard as u8 * 4)
+        + (argv.symbolic as u8 * 8)
+        + (argv.junction as u8 * 16);
 
     match cmd {
-        // Symbolic Link
-        8 => match winfs::symlink_file(&argv.src, &argv.dst) {
-            Ok(_) => println!(
+        // Junction
+        16 => match junction::create(&argv.dst, &argv.src) {
+            Ok(()) => println!(
                 "{} {:?}, {} {:?}",
-                "Symbolic Link created at the destination".bright_green(),
-                argv.dst.bright_green(),
-                "with the source path of".bright_green(),
-                argv.src.bright_green()
+                "Junction created at source".bright_green(),
+                argv.src.bright_green(),
+                "with the destination of",
+                argv.dst.bright_green()
             ),
-            Err(_) => abort(FILE_ERR, 2),
+            Err(_) => abort(PATH_ERR, 2),
         },
-
-        // Hard Link
-        4 => match fs::hard_link(&argv.src, &argv.dst) {
+        // Symbolic Link
+        8 => match winfs::symlink_file(&argv.dst, &argv.src) {
             Ok(_) => println!(
                 "{} {:?}, {} {:?}",
-                "Hard Link created at the destination".bright_green(),
-                argv.dst.bright_green(),
-                "with the source path of".bright_green(),
-                argv.src.bright_green()
+                "Symbolic Link created at source".bright_green(),
+                argv.src.bright_green(),
+                "with the destination of".bright_green(),
+                argv.dst.bright_green()
             ),
             Err(_) => abort(FILE_ERR, 3),
         },
 
-        // Soft Link
-        1 => match winfs::symlink_dir(&argv.src, &argv.dst) {
+        // Hard Link
+        4 => match fs::hard_link(&argv.dst, &argv.src) {
             Ok(_) => println!(
                 "{} {:?}, {} {:?}",
-                "Soft Link created at the destination".bright_green(),
-                argv.dst.bright_green(),
-                "with the source path of".bright_green(),
-                argv.src.bright_green()
+                "Hard Link created at source".bright_green(),
+                argv.src.bright_green(),
+                "with the destination of".bright_green(),
+                argv.dst.bright_green()
             ),
-            Err(_) => abort(PATH_ERR, 4),
+            Err(_) => abort(FILE_ERR, 4),
+        },
+
+        // Soft Link
+        1 => match winfs::symlink_dir(&argv.dst, &argv.src) {
+            Ok(_) => println!(
+                "{} {:?}, {} {:?}",
+                "Soft Link created at source".bright_green(),
+                argv.src.bright_green(),
+                "with the destination of".bright_green(),
+                argv.dst.bright_green()
+            ),
+            Err(_) => abort(PATH_ERR, 5),
         },
 
         // Invalid Link
-        _ => abort(LINK_TYPE_ERR, 4),
+        _ => abort(LINK_TYPE_ERR, 6),
     }
 }
 
